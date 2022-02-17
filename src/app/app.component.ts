f968d60d15656c479b3441ffd16f3472d2d42f6e
DVLaiger <vladmegera96@gmail.com>
2022-02-17 12:29:06 +0300

    5 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,83 +1,24 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { UserService } from './user.service';
-import { User } from './user';
+import { MatTableDataSource } from '@angular/material/table';
+import { Users } from '../assets/users.json';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UserService],
+  
 })
 export class AppComponent implements OnInit {
-  @ViewChild('readOnlyTemplate') readOnlyTemplate: TemplateRef<any>;
-  @ViewChild('editTemplate') editTemplate: TemplateRef<any>;
 
-  editedUser: User | null = null;
-  users: Array<User>;
-  isNewRecord: boolean;
-  statusMessage: string;
+  dataSource: MatTableDataSource<any>;
+  displayedColumns = ['id', 'name', 'age'];
 
-  constructor(private http: UserService) {
-    this.users = new Array<User>();
+  constructor() {}
+
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource();
+    this.dataSource.data = Users
   }
 
-  ngOnInit() {
-    this.loadUser();
-  }
-
-  private loadUser() {
-    this.http
-      .getUser()
-      .subscribe((data: any) => (this.users = data['workers']));
-  }
-
-  addUser() {
-    this.editedUser = new User(0, '', 0);
-    this.users.push(this.editedUser);
-    this.isNewRecord = true;
-  }
-
-  editUser(user: User) {
-    this.editedUser = new User(user.id, user.name, user.age);
-  }
-
-  loadTemplate(user: User) {
-    if (this.editedUser && this.editedUser.id === user.id) {
-      return this.editTemplate;
-    } else {
-      return this.readOnlyTemplate;
-    }
-  }
-
-  saveUser() {
-    if (this.isNewRecord) {
-      // добавляем пользователя
-      this.http.createUser(this.editedUser as User).subscribe((data) => {
-        (this.statusMessage = 'Данные успешно добавлены'), this.loadUser();
-      });
-      this.isNewRecord = false;
-      this.editedUser = null;
-    } else {
-      // изменяем пользователя
-      this.http.updateUser(this.editedUser as User).subscribe((data) => {
-        (this.statusMessage = 'Данные успешно обновлены'), this.loadUser();
-      });
-      this.editedUser = null;
-    }
-  }
-
-  cancel() {
-    // если отмена при добавлении, удаляем последнюю запись
-    if (this.isNewRecord) {
-      this.users.pop();
-      this.isNewRecord = false;
-    }
-    this.editedUser = null;
-  }
-  // удаление пользователя
-  deleteUser(user: User) {
-    this.http.deleteUser(user.id).subscribe((data) => {
-      (this.statusMessage = 'Данные успешно удалены'), this.loadUser();
-    });
-  }
-}
+}  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
- 
+import { MatTableModule } from '@angular/material/table'; 
 import { HttpClientModule }   from '@angular/common/http';
  
 @NgModule({
-    imports:      [ BrowserModule, FormsModule, HttpClientModule],
+    imports:      [ BrowserModule, FormsModule, HttpClientModule, MatTableModule],
     declarations: [ AppComponent],
     bootstrap:    [ AppComponent ]
 })
