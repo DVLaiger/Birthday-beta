7bb338efe94f1bd14af497489585211d6592f2d5
DVLaiger <86600531+DVLaiger@users.noreply.github.com>
2022-02-18 11:35:36 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { UserService } from '../services/user.service';
+import { MatPaginator } from '@angular/material/paginator';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [UserService],
-  
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements OnInit, AfterViewInit {
   dataSource: MatTableDataSource<any>;
   displayedColumns = ['id', 'name', 'age'];
 
-  constructor (private http: UserService) {} 
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  
 
+  constructor(private http: UserService) {}
 
   ngOnInit(): void {
-    this.loadUsers ();
+    this.loadUsers();
   }
 
   private loadUsers() {
-    this.http.getUser().subscribe((data: MatTableDataSource<any>)=> this.dataSource = data ["Users"])
+    this.http.getUser().subscribe((data: MatTableDataSource<any>) => (this.dataSource = data['Users']));
   }
 
-}  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
