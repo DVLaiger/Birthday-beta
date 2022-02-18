1cd4d3529cf02f2e9031bbb1f82d5709e0f7e438
DVLaiger <86600531+DVLaiger@users.noreply.github.com>
2022-02-18 11:34:46 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }   from './app.component';
-import { MatTableModule } from '@angular/material/table'; 
-import { HttpClientModule }   from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialModule} from '../material.module';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
  
 @NgModule({
-    imports:      [ BrowserModule, FormsModule, HttpClientModule, MatTableModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
     declarations: [ AppComponent],
     bootstrap:    [ AppComponent ]
 })
